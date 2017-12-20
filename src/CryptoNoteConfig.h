8f2404a925faa50781c8a5b0b65e9152bae42ee7
liincoin <34259730+liincoin@users.noreply.github.com>
2017-12-20 01:53:51 -0700

    Seed nodes added.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
-  //"your_seed_ip2.com:8080",
+"95.183.52.40:23555",  //"your_seed_ip1.com:8080",
+"198.23.206.26:23555",  //"your_seed_ip2.com:8080",
 };
 
 struct CheckpointData {
